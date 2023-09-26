51af9bdfcc45c96e32c03f3e92752625b53aabdc
mohamedshams <mohamedhames900@gmail.com>
2023-09-26 13:02:46 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,23 @@
+const menu = document.getElementById("menu");
+const navBar = document.querySelector(".navber");
+
+menu.addEventListener("click", (eo) => {
+  navBar.classList.toggle("active")
+}
+)
+    
+window.onscroll = () => {
+    navBar.classList.remove("active");
+}
+
+
+let dropdown_items = document.querySelectorAll(".items");
+
+dropdown_items.forEach(items => {
+    items.onclick = () => {
+        items_parent = items.parentElement.parentElement;
+        const output = items_parent.querySelector(".output");
+        output.value = items.innerText;
+    }
+})
+
